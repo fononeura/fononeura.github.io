6baab12e278a4f33ccd83f19e9040276660d5f58
Vagner dos Santos <vagner.dossantos@thoughtworks.com>
2020-02-15 23:07:09 -0300

    Updates

---
+++ precache-manifest.bd598f533d2788c253a9f4e36bd7508f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f14ca5d2b8f40c4a6e03165c7d5dd402",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dae43aa964af36dc62c7",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "56f3b5b2334e3d3ca173",
+    "url": "/static/js/2.0542ee8d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.0542ee8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dae43aa964af36dc62c7",
+    "url": "/static/js/main.1642afcb.chunk.js"
+  },
+  {
+    "revision": "d0371329dd54480745ea",
+    "url": "/static/js/runtime-main.ac216f2a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
